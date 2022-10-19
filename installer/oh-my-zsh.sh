a1d4e34a92fb5a76ed91a3f3bc822297b1ea7cc1
Jur93n <jurgen.kluft@gmail.com>
2022-10-19 13:16:50 +0800

    Update oh-my-zsh.sh

--- installer/oh-my-zsh.sh
+++ installer/oh-my-zsh.sh
@@ -10,9 +10,7 @@
 
 if [ -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
     echo "oh-my-zsh already installed!"
-    exit -1
+else
+    echo "Install oh-my-zsh ..."
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
-
-echo "Install oh-my-zsh ..."
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
